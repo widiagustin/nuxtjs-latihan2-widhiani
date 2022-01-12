a711dea3bb921314674902c77bdc9acfa2a27830
widhianiagustinn <widhianiagustin14@gmail.com>
2022-01-12 22:22:17 +0700

    update axios get data

--- pages/home.vue
+++ pages/home.vue
@@ -2,19 +2,26 @@
   <div class="items-center justify-center text-center pt-10">
     <h1 class="text-green-600 font-mono text-4xl">Welcome to this page!</h1>
     <p>for the contents</p>
+    <nuxt-link
+      v-for="post in posts"
+      :key="post.id"
+      class="list-data"
+      :to="'/post/' + post.id"
+    >
+      {{ post.title }}
+    </nuxt-link>
   </div>
 </template>
 <script>
+import axios from "axios";
+
 export default {
   name: "home",
   async asyncData() {
-    const api = "https://jsonplaceholder.typicode.com/posts";
-    const post = await axios.get(api).then((response) => {
-      return response.data;
-    });
-    return {
-      posts,
-    };
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    return { posts: data };
   },
 };
 </script>
