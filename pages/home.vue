db5d1323cb55c288e80fb390da4902e619159d69
widhianiagustinn <widhianiagustin14@gmail.com>
2022-01-13 21:26:29 +0700

    add content card

--- pages/home.vue
+++ pages/home.vue
@@ -1,15 +1,63 @@
 <template>
-  <div class="items-center justify-center text-center pt-10">
-    <h1 class="text-green-600 font-mono text-4xl">Welcome to this page!</h1>
-    <p>for the contents</p>
-    <nuxt-link
+  <div class=" justify-center pt-10">
+    <div class="items-center text-center">
+      <h1 class="text-green-600 font-mono text-4xl">Welcome to this page!</h1>
+    </div>
+    
+    <v-card
       v-for="post in posts"
       :key="post.id"
       class="list-data"
       :to="'/post/' + post.id"
     >
-      {{ post.title }}
-    </nuxt-link>
+        <div class="container flex flex-wrap justify-between mx-auto pt-6">
+          <div class="p-4 lg:w-1/3 w-1/2">
+            <div class="flex rounded-lg h-full bg-green-500 p-8 flex-col">
+              <div class="flex items-center mb-3">
+                <div
+                  class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0"
+                >
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                  </svg>
+                </div>
+                <h2 class="text-white text-lg title-font font-medium">
+                  {{ post.title }}
+                </h2>
+              </div>
+              <div class="flex-grow">
+                <p class="leading-relaxed text-base text-white">
+                  {{ post.body }}
+                </p>
+                <a
+                  href=""
+                  class="mt-3 text-indigo-800 hover:text-gray-900 inline-flex items-center"
+                  >Learn More
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+      </div>
+    </v-card>
   </div>
 </template>
 <script>
